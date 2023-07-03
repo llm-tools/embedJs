02b5c93e6e787addc1e70e55233013eb239c79a0
Adhityan KV <adhityan@hotmail.com>
2023-07-03 11:04:22 +0200

    cleanup examples

--- examples/pinecone/src/index.ts
+++ examples/pinecone/src/index.ts
@@ -18,8 +18,8 @@ const llmApplication = await new LLMApplicationBuilder()
 console.log(await llmApplication.query('What is paxos?'));
 // Paxos is an algorithm for implementing a fault-tolerant distributed system. It assumes a network of processes, each of which plays the role of proposer, acceptor, and learner. The algorithm chooses a leader, which plays the roles of the distinguished proposer and learner. The algorithm is used to reach consensus on a chosen value, and is obtained by the straightforward application of consensus to the state machine approach for building a distributed system.
 
-// console.log(await llmApplication.query('Why Does the M2 Mac Pro Exist?'));
+console.log(await llmApplication.query('Why Does the M2 Mac Pro Exist?'));
 // The Mac Pro exists to provide users with a powerful and expandable workstation-class computer.
 
-// console.log(await llmApplication.query('What is the best name for a company making colorful socks?'));
+console.log(await llmApplication.query('What is the best name for a company making colorful socks?'));
 // MrSocks
--- examples/simple/src/index.ts
+++ examples/simple/src/index.ts
@@ -1,12 +1,21 @@
 import 'dotenv/config';
 
-import { LLMApplicationBuilder, WebLoader } from '../../../src/index.js';
+import { LLMApplicationBuilder, PdfLoader, WebLoader, YoutubeLoader } from '../../../src/index.js';
 import { HNSWDb } from '../../../src/databases/hnswlib-db.js';
 
 const llmApplication = await new LLMApplicationBuilder()
+    .setSearchResultCount(30)
+    .addLoader(new PdfLoader({ url: 'https://lamport.azurewebsites.net/pubs/paxos-simple.pdf' }))
+    .addLoader(new YoutubeLoader({ videoIdOrUrl: 'https://www.youtube.com/watch?v=w2KbwC-s7pY' }))
     .addLoader(new WebLoader({ url: 'https://adhityan.com/' }))
     .setVectorDb(new HNSWDb())
     .build();
+
+console.log(await llmApplication.query('What is paxos?'));
+// Paxos is an algorithm for implementing a fault-tolerant distributed system. It assumes a network of processes, each of which plays the role of proposer, acceptor, and learner. The algorithm chooses a leader, which plays the roles of the distinguished proposer and learner. The algorithm is used to reach consensus on a chosen value, and is obtained by the straightforward application of consensus to the state machine approach for building a distributed system.
+
+console.log(await llmApplication.query('Why Does the M2 Mac Pro Exist?'));
+// The Mac Pro exists to provide users with access to PCI slots, as well as other unique features such as its design and performance capabilities.
 
 console.log(await llmApplication.query('Who is Adhityan?'));
 // Adhityan is a programmer, entrepreneur, and architect who is the Director of Engineering at Shift and has a presence on LinkedIn, GitHub, and Angel.co.
