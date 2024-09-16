7f55b75d2d0f39f03360ca89538932c34f514caf
Adhityan K V <adhityan@hotmail.com>
2024-09-16 11:18:27 +0200

    update google vertex ai embedding to use newer langchain version

--- src/embeddings/gecko-embeddings.ts
+++ src/embeddings/gecko-embeddings.ts
@@ -1,11 +1,11 @@
-import { GoogleVertexAIEmbeddings } from '@langchain/community/embeddings/googlevertexai';
+import { VertexAIEmbeddings } from '@langchain/google-vertexai';
 import { BaseEmbeddings } from '../interfaces/base-embeddings.js';
 
 export class GeckoEmbeddings implements BaseEmbeddings {
-    private model: GoogleVertexAIEmbeddings;
+    private model: VertexAIEmbeddings;
 
     constructor() {
-        this.model = new GoogleVertexAIEmbeddings({ model: 'textembedding-gecko', maxConcurrency: 3, maxRetries: 5 });
+        this.model = new VertexAIEmbeddings({ model: 'textembedding-gecko', maxConcurrency: 3, maxRetries: 5 });
     }
 
     async getDimensions(): Promise<number> {
